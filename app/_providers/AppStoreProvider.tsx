9e9ebddf8182c74f6de686eac21d70a88cbc2d55
Kaloyan Stefanov <kokmarok@gmail.com>
2024-11-28 19:30:25 +0200

    Initial commit

---
+++ app/_providers/AppStoreProvider.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import {ReactNode} from "react";
+
+type AppStoreProviderProps = {
+    children: ReactNode
+}
+
+export default function AppStoreProvider({children}: AppStoreProviderProps) {
+    return  
+}
